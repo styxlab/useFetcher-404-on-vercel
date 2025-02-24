0c1e0c5d203e02d70f9c909963ef3a5dcaeb6b0f
styxlab <styxlab@localhost.com>
2025-02-24 14:18:12 +0100

    test edge runtime on callback

--- app/routes/api.callback.ts
+++ app/routes/api.callback.ts
@@ -1,5 +1,7 @@
 import { ActionFunctionArgs } from "@vercel/remix";
 
+export const config = { runtime: 'edge' }
+
 export const action = async ({ request }: ActionFunctionArgs) => {
     console.log('action triggered!')
     return new Response('Hello, world!', {
